3d05dc6f76c3dbdd33a0dcce209005e5e2efc82a
cjsaud90 <cheonmyeongwon@gmail.com>
2020-09-15 22:40:26 +0900

    frist commit

---
+++ src/test/java/com/mwcheon/springbootcore/SpringbootcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwcheon.springbootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
