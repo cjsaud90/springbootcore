20f9ad35f4704974e6016cc0684dec4a7aff128e
DESKTOP-8R50E6R\brigh <cjsaud90@naver.com>
2020-09-20 09:24:37 +0900

    componentScan

--- src/main/java/com/mwcheon/springbootcore/SpringbootcoreApplication.java
+++ src/main/java/com/mwcheon/springbootcore/SpringbootcoreApplication.java
@@ -2,14 +2,19 @@ package com.mwcheon.springbootcore;
 
 import com.mwcheon.springbootcore.ioc.ApplicationConfig;
 import com.mwcheon.springbootcore.ioc.BookService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
+import java.awt.print.Book;
 import java.util.Arrays;
-
+//@SpringBootApplication ->  빈을 자동으로 등록 해준다. 스캔 위치 설정 ( 현재 패키지와 하위 패키지 )
 @SpringBootApplication
 public class SpringbootcoreApplication {
 
@@ -38,7 +43,14 @@ public class SpringbootcoreApplication {
 
 //        TODO 5. 그래서 나온게 SpringBootApplication을 쓴다 모든게 다 들어 있음
 
-
+        //@ Component와 컴포넌트 스캔
+        //TODO 빈을 등록하는 또 다란 방법 펑션을 사용한 빈 등록 ( 비추 )
+//        var app = new SpringBootApplication(SpringbootcoreApplication.class);
+//        app.addInitializers((new ApplicationContextInitializer<GenericApplicationContext>) application ->  {
+//          application.registerBean(BookService.class);
+//          application.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("FUNCTAIONAL REGISTER BEAN"));
+//        })
+//        app.run(args);
 //
 
    }
--- src/main/java/com/mwcheon/springbootcore/autowire/BookService.java
+++ src/main/java/com/mwcheon/springbootcore/autowire/BookService.java
@@ -4,6 +4,9 @@ package com.mwcheon.springbootcore.autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 @Service
 public class BookService  {
 
@@ -16,11 +19,19 @@ public class BookService  {
 //     this.bookRepository = bookRepository;
 // }
 //
+    // primary 에 해당하는 레파지토리 빈을 가져온다
     @Autowired
     BookRepository bookRepository;
 
+    // 모두 다 가져오려면
+    @Autowired
+    List<BookRepository> bookRepositories;
+
     public void printBookRepostory(){
+        this.bookRepositories.forEach(System.out::println);
         System.out.println(bookRepository.getClass());
     }
+
+//    @PostConstruct
 
 }
